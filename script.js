7c5fb43ed86997c03535976836fcbe9ce3411305
Vishoo Verma <78697370+vishoov@users.noreply.github.com>
2026-04-23 13:51:01 +0530

    created a submission

--- script.js
+++ script.js
@@ -3,8 +3,25 @@ getSumBtn.append("Get Total Price");
 document.body.appendChild(getSumBtn);
 
 const getSum = () => {
-//Add your code here
-  
+
+	const prices = document.querySelectorAll('.price');
+
+	let total = 0;
+
+	prices.forEach(function(el){
+		total+= parseInt(el.textContent);
+	})
+
+	// create a new row, then a new cell and then set the price
+	const newRow = document.createElement('tr');
+
+	const totalCell = document.createElement('td');
+	totalCell.textContent=`Total = `+total;
+
+	newRow.appendChild(totalCell)
+	document.querySelector('table').appendChild(newRow);
+
+	
 };
 
 getSumBtn.addEventListener("click", getSum);
